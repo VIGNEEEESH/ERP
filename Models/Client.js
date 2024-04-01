fccbb27b6108b7af93a20a1420a3e187e3ca265b
Vignesh <103361940+VIGNEEEESH@users.noreply.github.com>
2024-04-01 15:05:28 +0200

    updated client schema

--- Models/Client.js
+++ Models/Client.js
@@ -4,6 +4,8 @@ const Schema = mongoose.Schema;
 const clientSchema = new Schema({
   clientName: { type: String, required: true },
   companyName: { type: String, required: true },
+  mobile: { type: String, required: true },
+  email: { type: String, required: true },
   projects: [{ type: String, required: true }],
 });
 
