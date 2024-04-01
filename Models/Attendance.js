0317098d811502b415acde1a19db99b8117c0d9a
Vignesh <103361940+VIGNEEEESH@users.noreply.github.com>
2024-04-01 14:25:33 +0200

    created attendance schema

---
+++ Models/Attendance.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const uniqueValidator = require("mongoose-unique-validator");
+
+const attendanceSchema = new Schema({
+  attendanceStatus: { type: String, required: true },
+  date: { type: String, required: true, unique: true },
+  loggedInTime: { type: String, required: true },
+  loggedOutTime: { type: String, required: true },
+  workStatus: { type: String, required: true },
+  userId: { type: String, required: true },
+});
+attendanceSchema.plugin(uniqueValidator);
+module.exports = mongoose.model("Attendance", attendanceSchema);
