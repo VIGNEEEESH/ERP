4ad2cd992b9fd76400bbc1796726dd583140b5e2
Vignesh <103361940+VIGNEEEESH@users.noreply.github.com>
2024-04-01 14:28:40 +0200

    Created work schema

---
+++ Models/Work.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const uniqueValidator = require("mongoose-unique-validator");
+
+const workSchema = new Schema({
+  date: { type: String, required: true, unique: true },
+  workDone: { type: String, required: true },
+  userId: { type: String, required: true },
+});
+workSchema.plugin(uniqueValidator);
+module.exports = mongoose.model("Work", workSchema);
