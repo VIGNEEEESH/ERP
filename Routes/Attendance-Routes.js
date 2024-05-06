96aa59237eee64e1b322da14c3ed66ce7d8070c5
Vignesh <103361940+VIGNEEEESH@users.noreply.github.com>
2024-05-06 18:42:06 +0200

    Added authorization to attendance routes

--- Routes/Attendance-Routes.js
+++ Routes/Attendance-Routes.js
@@ -4,21 +4,29 @@ const router = express.Router();
 const attendanceControllers = require("../Controllers/Attendance-Controllers");
 const checkAuth = require("../Middleware/check-auth");
 
-router.get("/get/all/attendance", attendanceControllers.getAllAttendance);
+router.get(
+  "/get/all/attendance",
+  checkAuth(["CEO", "HR", "DeptHead", "Employee"]),
+  attendanceControllers.getAllAttendance
+);
 router.get(
   "/get/attendance/bydate/:date",
+  checkAuth(["CEO", "HR", "DeptHead", "Employee"]),
   attendanceControllers.getAttendanceByDate
 );
 router.get(
   "/get/attendance/bydateanduserid/:date/:userId",
+  checkAuth(["CEO", "HR", "DeptHead", "Employee"]),
   attendanceControllers.getAttendanceByDateAndUserId
 );
 router.get(
   "/get/attendance/byuserId/:userId",
+  checkAuth(["CEO", "HR", "DeptHead", "Employee"]),
   attendanceControllers.getAttendanceByUserId
 );
 router.post(
   "/create/attendance",
+  checkAuth(["CEO", "HR", "DeptHead", "Employee"]),
   [
     check("attendanceStatus").isLength({ min: 2, max: 255 }),
     check("userId").notEmpty(),
@@ -28,6 +36,7 @@ router.post(
 );
 router.patch(
   "/update/workstatus",
+  checkAuth(["CEO", "HR", "DeptHead", "Employee"]),
   [
     check("workStatus").isLength({ min: 2, max: 255 }),
     check("userId").notEmpty(),
@@ -37,6 +46,7 @@ router.patch(
 );
 router.patch(
   "/add/loggedouttime",
+  checkAuth(["CEO", "HR", "DeptHead", "Employee"]),
   [check("userId").notEmpty()],
   attendanceControllers.addLoggedOutTime
 );
