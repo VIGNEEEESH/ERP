12d2f79bf4fc7c8f1e29b4d19ca50744332b2253
Vignesh <103361940+VIGNEEEESH@users.noreply.github.com>
2024-05-18 10:08:01 +0200

    Updated the ceo and hr dashboard

--- src/pages/hrdashboard/home.jsx
+++ src/pages/hrdashboard/home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import {
   Typography,
   Card,
@@ -26,9 +26,59 @@ import {
   ordersOverviewData,
 } from "@/data";
 import { CalendarDaysIcon, ChartBarIcon, CheckCircleIcon, ClipboardDocumentCheckIcon, ClipboardDocumentIcon, ClipboardDocumentListIcon, ClockIcon, FolderIcon, UserCircleIcon, UserIcon, UsersIcon } from "@heroicons/react/24/solid";
-
+import { AuthContext } from "../auth/Auth-context";
+import { message } from "antd";
 
 export function Home() {
+  const auth=useContext(AuthContext)
+  const [employees,setEmployees]=useState([])
+  const [clients,setClients]=useState([])
+  const [attendance,setAttendance]=useState([])
+  const [projects,setProjects]=useState([])
+  const [tasks,setTasks]=useState([])
+  const [leaves,setLeaves]=useState([])
+  const [products,setProducts]=useState([])
+  const [departments,setDepartments]=useState([])
+  const currentDate = new Date();
+  const formattedDate = currentDate.toISOString().split("T")[0];
+  useEffect(()=>{
+    const fetchData=async()=>{
+      try{
+    const employeeResponse=await fetch(`${import.meta.env.REACT_APP_BACKEND_URL}/api/erp/user/get/all/users`,{headers:{Authorization:"Bearer "+auth.token}})
+    const clientResponse=await fetch(`${import.meta.env.REACT_APP_BACKEND_URL}/api/erp/client/get/all/clients`,{headers:{Authorization:"Bearer "+auth.token}})
+    const clockInEmployeeResponse=await fetch(`${import.meta.env.REACT_APP_BACKEND_URL}/api/erp/attendance/get/attendance/bydate/${formattedDate}`,{headers:{Authorization:"Bearer "+auth.token}})
+    const projectResponse=await fetch(`${import.meta.env.REACT_APP_BACKEND_URL}/api/erp/project/get/all/projects`,{headers:{Authorization:"Bearer "+auth.token}})
+    const productResponse=await fetch(`${import.meta.env.REACT_APP_BACKEND_URL}/api/erp/product/get/all/products`,{headers:{Authorization:"Bearer "+auth.token}})
+    const taskResponse=await fetch(`${import.meta.env.REACT_APP_BACKEND_URL}/api/erp/task/get/all/tasks`,{headers:{Authorization:"Bearer "+auth.token}})
+    const leaveResponse=await fetch(`${import.meta.env.REACT_APP_BACKEND_URL}/api/erp/leave/get/all/leaves`,{headers:{Authorization:"Bearer "+auth.token}})
+    const departmentResponse=await fetch(`${import.meta.env.REACT_APP_BACKEND_URL}/api/erp/department/get/all/departments`,{headers:{Authorization:"Bearer "+auth.token}})
+
+    if (!employeeResponse.ok || !clientResponse.ok || !clockInEmployeeResponse.ok || !projectResponse.ok || !taskResponse.ok|| !leaveResponse.ok || !productResponse.ok || !departmentResponse.ok) {
+      throw new Error(`Failed to fetch data`);
+  }
+  const employeeData = await employeeResponse.json();
+  const clientData = await clientResponse.json();
+  const clockInEmployeeData = await clockInEmployeeResponse.json();
+  const projectData = await projectResponse.json();
+  const productData = await productResponse.json();
+  const taskData = await taskResponse.json();
+  const leaveData = await leaveResponse.json();
+  const departmentData = await departmentResponse.json();
+  
+  setEmployees(employeeData.users);
+  setClients(clientData.clients);
+  setAttendance(clockInEmployeeData.attendance);
+  setProjects(projectData.projects);
+  setTasks(taskData.tasks);
+  setLeaves(leaveData.leaves);
+  setProducts(productData.products);
+  setDepartments(departmentData.departments);
+    }  catch(err){
+      message.error("Error fetching data", err.message);
+    }
+  }
+    fetchData()
+  },[auth.token])
   return (
     <div className="mt-12">
       <div className="mb-12 grid gap-y-10 gap-x-6 md:grid-cols-2 xl:grid-cols-4">
@@ -36,61 +86,62 @@ export function Home() {
         title="Total Employees"
         icon={<UserCircleIcon className="w-8 h-8"/>}
         color="gray"
-        value="50"
+        value={employees.length}
         />
         <StatisticsCard
         title="Employees Clocked In"
         icon={<UsersIcon className="w-8 h-8"/>}
         color="gray"
-        value="50"
+        value={attendance.filter(attendance => !attendance.loggedOutTime  ).length}
         />
-        <StatisticsCard
-        title="Projects Ongoing"
-        icon={<ChartBarIcon className="w-8 h-8"/>}
-        color="gray"
-        value="50"
-        />
+       <StatisticsCard
+    title="Projects Ongoing"
+    icon={<ChartBarIcon className="w-8 h-8"/>}
+    color="gray"
+    value={projects.filter(project => project.progress !== 100).length}
+/>
+
         <StatisticsCard
         title="Tasks Pending"
         icon={<ClipboardDocumentListIcon className="w-8 h-8"/>}
         color="gray"
-        value="50"
+        value={tasks.filter(task => task.progress !== 100).length}
         />
         <StatisticsCard
         title="Total Clients"
         icon={<UserCircleIcon className="w-8 h-8"/>}
         color="gray"
-        value="50"
+        value={clients.length}
         />
         <StatisticsCard
         title="Leaves Pending"
         icon={<CalendarDaysIcon className="w-8 h-8"/>}
         color="gray"
-        value="50"
+        value={leaves.filter(leave => leave.status !="Approved").length}
         />
         <StatisticsCard
         title="No of Products"
         icon={<FolderIcon className="w-8 h-8"/>}
         color="gray"
-        value="50"
+        value={products.length}
         />
         <StatisticsCard
         title="No of Departments"
         icon={<UserCircleIcon className="w-8 h-8"/>}
         color="gray"
-        value="50"
+        value={departments.length}
         />
         <StatisticsCard
         title="People Avalible"
         icon={<UserIcon className="w-8 h-8"/>}
         color="gray"
-        value="50"
+        value={attendance.filter(attendance => attendance.workStatus == "Available").length}
         />
         <StatisticsCard
         title="Completed Projects"
         icon={<ClipboardDocumentCheckIcon className="w-8 h-8"/>}
         color="gray"
-        value="50"
+        value={projects.filter(project => project.progress == 100).length}
         />
       </div>
       <div className="mb-6 grid grid-cols-1 gap-y-12 gap-x-6 md:grid-cols-2 xl:grid-cols-3">
@@ -109,192 +160,6 @@ export function Home() {
             }
           />
         ))}
-      </div>
-      <div className="mb-4 grid grid-cols-1 gap-6 xl:grid-cols-3">
-        <Card className="overflow-hidden xl:col-span-2 border border-blue-gray-100 shadow-sm">
-          <CardHeader
-            floated={false}
-            shadow={false}
-            color="transparent"
-            className="m-0 flex items-center justify-between p-6"
-          >
-            <div>
-              <Typography variant="h6" color="blue-gray" className="mb-1">
-                Projects
-              </Typography>
-              <Typography
-                variant="small"
-                className="flex items-center gap-1 font-normal text-blue-gray-600"
-              >
-                <CheckCircleIcon strokeWidth={3} className="h-4 w-4 text-blue-gray-200" />
-                <strong>30 done</strong> this month
-              </Typography>
-            </div>
-            <Menu placement="left-start">
-              <MenuHandler>
-                <IconButton size="sm" variant="text" color="blue-gray">
-                  <EllipsisVerticalIcon
-                    strokeWidth={3}
-                    fill="currenColor"
-                    className="h-6 w-6"
-                  />
-                </IconButton>
-              </MenuHandler>
-              <MenuList>
-                <MenuItem>Action</MenuItem>
-                <MenuItem>Another Action</MenuItem>
-                <MenuItem>Something else here</MenuItem>
-              </MenuList>
-            </Menu>
-          </CardHeader>
-          <CardBody className="overflow-x-scroll px-0 pt-0 pb-2">
-            <table className="w-full min-w-[640px] table-auto">
-              <thead>
-                <tr>
-                  {["companies", "members", "budget", "completion"].map(
-                    (el) => (
-                      <th
-                        key={el}
-                        className="border-b border-blue-gray-50 py-3 px-6 text-left"
-                      >
-                        <Typography
-                          variant="small"
-                          className="text-[11px] font-medium uppercase text-blue-gray-400"
-                        >
-                          {el}
-                        </Typography>
-                      </th>
-                    )
-                  )}
-                </tr>
-              </thead>
-              <tbody>
-                {projectsTableData.map(
-                  ({ img, name, members, budget, completion }, key) => {
-                    const className = `py-3 px-5 ${
-                      key === projectsTableData.length - 1
-                        ? ""
-                        : "border-b border-blue-gray-50"
-                    }`;
-
-                    return (
-                      <tr key={name}>
-                        <td className={className}>
-                          <div className="flex items-center gap-4">
-                            <Avatar src={img} alt={name} size="sm" />
-                            <Typography
-                              variant="small"
-                              color="blue-gray"
-                              className="font-bold"
-                            >
-                              {name}
-                            </Typography>
-                          </div>
-                        </td>
-                        <td className={className}>
-                          {members.map(({ img, name }, key) => (
-                            <Tooltip key={name} content={name}>
-                              <Avatar
-                                src={img}
-                                alt={name}
-                                size="xs"
-                                variant="circular"
-                                className={`cursor-pointer border-2 border-white ${
-                                  key === 0 ? "" : "-ml-2.5"
-                                }`}
-                              />
-                            </Tooltip>
-                          ))}
-                        </td>
-                        <td className={className}>
-                          <Typography
-                            variant="small"
-                            className="text-xs font-medium text-blue-gray-600"
-                          >
-                            {budget}
-                          </Typography>
-                        </td>
-                        <td className={className}>
-                          <div className="w-10/12">
-                            <Typography
-                              variant="small"
-                              className="mb-1 block text-xs font-medium text-blue-gray-600"
-                            >
-                              {completion}%
-                            </Typography>
-                            <Progress
-                              value={completion}
-                              variant="gradient"
-                              color={completion === 100 ? "green" : "blue"}
-                              className="h-1"
-                            />
-                          </div>
-                        </td>
-                      </tr>
-                    );
-                  }
-                )}
-              </tbody>
-            </table>
-          </CardBody>
-        </Card>
-        <Card className="border border-blue-gray-100 shadow-sm">
-          <CardHeader
-            floated={false}
-            shadow={false}
-            color="transparent"
-            className="m-0 p-6"
-          >
-            <Typography variant="h6" color="blue-gray" className="mb-2">
-              Orders Overview
-            </Typography>
-            <Typography
-              variant="small"
-              className="flex items-center gap-1 font-normal text-blue-gray-600"
-            >
-              <ArrowUpIcon
-                strokeWidth={3}
-                className="h-3.5 w-3.5 text-green-500"
-              />
-              <strong>24%</strong> this month
-            </Typography>
-          </CardHeader>
-          <CardBody className="pt-0">
-            {ordersOverviewData.map(
-              ({ icon, color, title, description }, key) => (
-                <div key={title} className="flex items-start gap-4 py-3">
-                  <div
-                    className={`relative p-1 after:absolute after:-bottom-6 after:left-2/4 after:w-0.5 after:-translate-x-2/4 after:bg-blue-gray-50 after:content-[''] ${
-                      key === ordersOverviewData.length - 1
-                        ? "after:h-0"
-                        : "after:h-4/6"
-                    }`}
-                  >
-                    {React.createElement(icon, {
-                      className: `!w-5 !h-5 ${color}`,
-                    })}
-                  </div>
-                  <div>
-                    <Typography
-                      variant="small"
-                      color="blue-gray"
-                      className="block font-medium"
-                    >
-                      {title}
-                    </Typography>
-                    <Typography
-                      as="span"
-                      variant="small"
-                      className="text-xs font-medium text-blue-gray-500"
-                    >
-                      {description}
-                    </Typography>
-                  </div>
-                </div>
-              )
-            )}
-          </CardBody>
-        </Card>
       </div>
     </div>
   );
