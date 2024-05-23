e5c7fab21afa9d2813c888f524b58f9f3cb70e8a
Vignesh <103361940+VIGNEEEESH@users.noreply.github.com>
2024-05-23 11:31:58 +0200

    updated the port

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ mongoose
   .connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rw3waqy.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
-  .then(app.listen(4444))
+  .then(app.listen(80))
   .catch((err) => {
     console.log(err);
   });
