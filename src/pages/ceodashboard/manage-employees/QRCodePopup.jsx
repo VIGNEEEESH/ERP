cd7876c1a282c09bfc56576b7fe636a119f2d19f
Aditi Baggu <aditibaggu@gmail.com>
2024-05-31 17:26:19 +0530

    QR code changes

--- src/pages/ceodashboard/manage-employees/QRCodePopup.jsx
+++ src/pages/ceodashboard/manage-employees/QRCodePopup.jsx
@@ -1,21 +1,33 @@
 import React from 'react';
 import QRCode from 'qrcode.react';
+import { XIcon } from '@heroicons/react/solid';
 
 const QRCodePopup = ({ qrCodeData, onClose }) => {
-    const handleCopyUserId = () => {
-        navigator.clipboard.writeText(qrCodeData.userId);
-      };
+  const handleCopyUserId = () => {
+    navigator.clipboard.writeText(qrCodeData.userId).then(() => {
+      alert("User ID copied to clipboard");
+    }).catch((err) => {
+      alert("Failed to copy User ID: ", err);
+    });
+  };
+
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-500 bg-opacity-50 z-50">
-      <div className="bg-white p-10 rounded-lg">
-        <QRCode size={500} value={qrCodeData.userId} />
-        <button className="mt-4 bg-blue-500 hover:bg-blue-600 text-white py-2 px-10 rounded" onClick={onClose}>
-          Close
-        </button><br /><br />
-        <div className="flex items-center">
-          <span className="mr-2">{qrCodeData.userId}</span>
-          <button onClick={handleCopyUserId} className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded">
-            Copy UserId
+      <div className="bg-white p-8 rounded-lg relative">
+        <QRCode size={300} value={qrCodeData.userId} />
+        <button 
+          className="absolute top-2 right-2 text-gray-500 hover:text-gray-700" 
+          onClick={onClose}
+        >
+          <XIcon className="h-6 w-6" />
+        </button>
+        <div className="mt-6 flex items-center justify-between">
+          <span className="text-gray-700">{qrCodeData.userId}</span>
+          <button 
+            onClick={handleCopyUserId} 
+            className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded"
+          >
+            Copy UID
           </button>
         </div>
       </div>
