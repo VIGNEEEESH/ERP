31b1275d6d70185ddaaf7de6d78dc0c2a7778dae
Aditi Baggu <aditibaggu@gmail.com>
2024-06-12 09:56:30 +0530

    Myoffice is updated in all dashboards

--- src/pages/ceodashboard/myOffice/MyOffice.jsx
+++ src/pages/ceodashboard/myOffice/MyOffice.jsx
@@ -170,111 +170,110 @@ export function MyOffice() {
                     <div className="flex items-center">
                         <Typography variant="h6" color="white" className="mr-4">
                             My Office
-                        </Typography>
-                        
-                    </div>
-                    <div className="ml-auto">
-                        <Button
-                            color="green"
-                            disabled={isClockedIn === "Present"}
-                            onClick={() => setConfirmModalVisible(true)}
-                        >
-                            Clock In
-                        </Button>
-                        <Button
-                            color="red"
-                            disabled={isClockedOut || !isClockedIn}
-                            onClick={() => setConfirmClockOutModalVisible(true)}
-                            className="ml-4"
-                        >
-                            Clock Out
-                        </Button>
-                    </div>
-                </CardHeader>
-                <CardBody className="overflow-x-scroll px-0 pt-0 pb-2">
-                    <table className="w-full min-w-[640px] table-auto">
-                        <thead>
-                            <tr>
-                                {["Name", "Role", "Status", "Time"].map((el) => (
-                                    <th key={el} className="border-b border-blue-gray-50 py-3 px-5 text-left">
-                                        <Typography variant="small" className="text-[11px] font-bold uppercase text-blue-gray-400">
-                                            {el}
-                                        </Typography>
-                                    </th>
-                                ))}
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {data.map(({ employee, ...att }, key) => {
-                                const className = `py-3 px-5 ${key === data.length - 1 ? "" : "border-b border-blue-gray-50"}`;
+                            </Typography>
+                </div>
+                <div className="ml-auto flex flex-col md:flex-row items-center">
+                    <Button
+                        color="green"
+                        disabled={isClockedIn === "Present"}
+                        onClick={() => setConfirmModalVisible(true)}
+                        className="mb-2 md:mb-0 md:mr-4"
+                    >
+                        Clock In
+                    </Button>
+                    <Button
+                        color="red"
+                        disabled={isClockedOut || !isClockedIn}
+                        onClick={() => setConfirmClockOutModalVisible(true)}
+                    >
+                        Clock Out
+                    </Button>
+                </div>
+            </CardHeader>
+            <CardBody className="overflow-x-scroll px-0 pt-0 pb-2">
+                <table className="w-full min-w-[640px] table-auto">
+                    <thead>
+                        <tr>
+                            {["Name", "Role", "Status", "Time"].map((el) => (
+                                <th key={el} className="border-b border-blue-gray-50 py-3 px-5 text-left">
+                                    <Typography variant="small" className="text-[11px] font-bold uppercase text-blue-gray-400">
+                                        {el}
+                                    </Typography>
+                                </th>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.map(({ employee, ...att }, key) => {
+                            const className = `py-3 px-5 ${key === data.length - 1 ? "" : "border-b border-blue-gray-50"}`;
 
-                                return (
-                                    <tr key={key}>
-                                        <td className={className}>
-                                            <div className="flex items-center gap-4">
-                                                <Avatar src={`${import.meta.env.REACT_APP_BACKEND_URL}/${employee.image}`} alt={employee.name} size="sm" variant="rounded" />
-                                                <div>
-                                                    <Typography variant="small" color="blue-gray" className="font-semibold">
-                                                        {employee.firstName}&nbsp;{employee.lastName}
-                                                    </Typography>
-                                                    <Typography className="text-xs font-normal text-blue-gray-500">
-                                                        {employee.email}
-                                                    </Typography>
-                                                </div>
+                            return (
+                                <tr key={key}>
+                                    <td className={className}>
+                                        <div className="flex items-center gap-4">
+                                            <Avatar src={`${import.meta.env.REACT_APP_BACKEND_URL}/${employee.image}`} alt={employee.name} size="sm" variant="rounded" />
+                                            <div>
+                                                <Typography variant="small" color="blue-gray" className="font-semibold">
+                                                    {employee.firstName}&nbsp;{employee.lastName}
+                                                </Typography>
+                                                <Typography className="text-xs font-normal text-blue-gray-500">
+                                                    {employee.email}
+                                                </Typography>
                                             </div>
-                                        </td>
-                                        <td className={className}>
-                                            <Typography className="text-xs font-semibold text-blue-gray-600">
-                                                {employee.role}
-                                            </Typography>
-                                        </td>
-                                        <td className={className}>
-                                            <Chip variant="gradient" value={att.workStatus} className="py-0.5 px-2 text-[11px] font-medium w-fit" />
-                                        </td>
-                                        <td className={className}>
-                                            <Typography as="a" href="#" className="text-xs font-semibold text-blue-gray-600">
-                                                Last clocked in at {att.loggedInTime} {att.loggedOutTime && `& clocked out at ${att.loggedOutTime}`}
-                                            </Typography>
-                                        </td>
-                                    </tr>
-                                );
-                            })}
-                        </tbody>
-                    </table>
-                </CardBody>
-            </Card>
-            
-            <Modal  
-                title="Confirm Clock In"
-                visible={confirmModalVisible}
-                onOk={handleConfirmClockIn}
-                onCancel={handleCancelClockIn}
-                okText="Yes"
-                okType='default'
-                cancelText="Cancel"
-            >
-                Are you sure you want to clock in?
-            </Modal>
-            <Modal 
-                title="Confirm Clock Out"
-                visible={confirmClockOutModalVisible}
-                onOk={handleConfirmClockOut}
-                onCancel={handleCancelClockOut}
-                okText="Yes"
-                okType='default'
-                cancelText="Cancel"
-            >
-                <div>Are you sure you want to clock out?</div>
-                <Input.TextArea
-                    placeholder="Enter the work done for the day"
-                    value={workDone}
-                    onChange={(e) => setWorkDone(e.target.value)}
-                    rows={4}
-                    className="mt-4"
-                />
-            </Modal>
-        </div>
-    );
+                                        </div>
+                                    </td>
+                                    <td className={className}>
+                                        <Typography className="text-xs font-semibold text-blue-gray-600">
+                                            {employee.role}
+                                        </Typography>
+                                    </td>
+                                    <td className={className}>
+                                        <Chip variant="gradient" value={att.workStatus} className="py-0.5 px-2 text-[11px] font-medium w-fit" />
+                                    </td>
+                                    <td className={className}>
+                                        <Typography as="a" href="#" className="text-xs font-semibold text-blue-gray-600">
+                                            Last clocked in at {att.loggedInTime} {att.loggedOutTime && `& clocked out at ${att.loggedOutTime}`}
+                                        </Typography>
+                                    </td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </CardBody>
+        </Card>
+        
+        <Modal  
+            title="Confirm Clock In"
+            visible={confirmModalVisible}
+            onOk={handleConfirmClockIn}
+            onCancel={handleCancelClockIn}
+            okText="Yes"
+            okType='default'
+            cancelText="Cancel"
+        >
+            Are you sure you want to clock in?
+        </Modal>
+        <Modal 
+            title="Confirm Clock Out"
+            visible={confirmClockOutModalVisible}
+            onOk={handleConfirmClockOut}
+            onCancel={handleCancelClockOut}
+            okText="Yes"
+            okType='default'
+            cancelText="Cancel"
+        >
+            <div>Are you sure you want to clock out?</div>
+            <Input.TextArea
+                placeholder="Enter the work done for the day"
+                value={workDone}
+                onChange={(e) => setWorkDone(e.target.value)}
+                rows={4}
+                className="mt-4"
+            />
+        </Modal>
+    </div>
+);
 }
 
 export default MyOffice;
