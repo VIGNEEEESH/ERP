42a7e957ad51917a984db688d9b6b9757c90e1b2
Vignesh <103361940+VIGNEEEESH@users.noreply.github.com>
2024-07-04 20:17:49 +0530

    Updated limit of payload in app.js

--- app.js
+++ app.js
@@ -20,7 +20,10 @@ const messageRoutes = require("./Routes/Message-Routes");
 const chatRoutes = require("./Routes/Chat-Routes");
 const fileRoutes = require("./Routes/File-Routes");
 const path = require("path");
-app.use(bodyParser.json());
+
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+
 app.use(cors());
 app.use("/uploads/images", express.static(path.join("uploads", "images")));
 
